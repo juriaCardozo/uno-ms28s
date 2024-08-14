15fb0fcf7007d557fd25c9d0d0741e4c48aa8a7e
Eduardo Viega <eduardoviega12@gmail.com>
2024-08-14 01:27:47 -0300

    FEAT - Símbolos das cartas alteradas
    Foi alterado o símbolo das cartas Reverse, Skip, ColorPicker e das cartas de compras.

--- src/Interfaces/UNOConstants_Original.java
+++ src/Interfaces/UNOConstants_Original.java
@@ -10,17 +10,13 @@ public interface UNOConstants_Original {
 	public static int NUMBERS = 1;
 	public static int ACTION = 2;
 	public static int WILD = 3;
-	
-	//ActionCard Characters
-	Character charREVERSE = (char) 8634;							//Decimal
-	Character charSKIP    = (char) Integer.parseInt("2718",16); 	//Unicode
-	
+
 	//ActionCard Functions
-	String REVERSE = charREVERSE.toString();
-	String SKIP	= charSKIP.toString();
-	String DRAW2PLUS = "2+";
+	String REVERSE = "\uD83D\uDDD8";	//Unicode
+	String SKIP	= "\uD83D\uDEC7";		//Unicode
+	String DRAW2PLUS = "+2";
 	
 	//Wild card functions
-	String W_COLORPICKER = "W";
-	String W_DRAW4PLUS = "4+";	
+	String W_COLORPICKER = "\uD83C\uDF10";
+	String W_DRAW4PLUS = "+4";
 }
