eef53eb16e8ea586e3407e4ca5fc2d9c2a6cb7d5
Júlia Cardozo <ccardozojulia@gmail.com>
2024-08-18 04:06:04 +0000

    Botão de ajuda

--- src/View/InfoPanel.java
+++ src/View/InfoPanel.java
@@ -1,11 +1,13 @@
 package View;
+import Interfaces.UNOConstants;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 /*
 Code created by Josh Braza 
 */
@@ -19,16 +21,41 @@ public class InfoPanel extends JPanel {
 	private int you = 0;
 	private int pc = 0;
 	private int rest = 0;
+
+	private final JButton helpButton;
 	
 	public InfoPanel(){
-		setPreferredSize(new Dimension(275,200));
+		setPreferredSize(new Dimension(275,250));
 		setOpaque(false);
 		error = "";
 		text = "O jogo começou";
 		
+		helpButton = new RoundedJButton("Ajuda", new UNOConstants().getBLUE(""), 20);
+		helpButton.setFocusable(false);
+		helpButton.addActionListener(e -> showHelpDialog());
+		add(helpButton);
+		setLayout(null);
+
 		updateText(text);
 	}
-	
+
+	private void showHelpDialog() {
+
+		String regras = """
+			Regras do Uno:
+				1. Cada jogador começa com 7 cartas.
+				2. O objetivo é ser o primeiro a se livrar de todas as suas cartas.
+				3. No seu turno, você pode jogar uma carta que corresponda à cor ou ao número da carta no centro do jogo.
+				4. Se você não puder jogar uma carta, deve comprar uma do baralho.
+				5. Existem cartas especiais que podem mudar a cor ou pular a vez do próximo jogador.
+				6. Quando tiver duas cartas na mão, clique no botão de dizer "UNO!" e jogue sua carta.
+				7. Se um jogador não disser "UNO!", ele deve comprar duas cartas como penalidade.
+				8. O jogo continua até que um jogador fique sem cartas.
+		""";
+
+		JOptionPane.showMessageDialog(this, regras, "Regras do Jogo", JOptionPane.INFORMATION_MESSAGE);
+	}
+
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		panelCenter = getWidth()/2;
@@ -36,6 +63,12 @@ public class InfoPanel extends JPanel {
 		printMessage(g);
 		printError(g);
 		printDetail(g);
+
+		int buttonWidth = 120;
+		int buttonHeight = 30;
+		int xPos = panelCenter - buttonWidth / 2;
+		int yPos = 200;
+		helpButton.setBounds(xPos, yPos, buttonWidth, buttonHeight);
 	}
 
 	private void printError(Graphics g) {
@@ -76,7 +109,7 @@ public class InfoPanel extends JPanel {
 		int xPos = panelCenter - fm.stringWidth(text) / 2;
 		
 		g.setFont(adjustedFont);
-		g.drawString(text, xPos, 120);
+		g.drawString(text, xPos, 115);
 		
 		text = "Cartas restantes: " + rest;
 		xPos = panelCenter - fm.stringWidth(text) / 2;
